1cc6fcf8e4d9ca81dd7189a7ebf4fb02120745b4
bruno.pereira <bruno.cardozo.pereira@gmail.com>
2023-03-15 15:14:33 -0300

    feat: basic project setup

---
+++ src/main/java/me/brunocardozo/meusgastos/MeusgastosApplication.java
@@ -0,0 +1,13 @@
+package me.brunocardozo.meusgastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusgastosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeusgastosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/brunocardozo/meusgastos/MeusgastosApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunocardozo.meusgastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusgastosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
