a289524ef421f1ce0e4c4abb7cf017335e7346e5
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-03-16 22:45:08 -0300

    feat: UsuarioRepository

--- src/main/java/me/brunocardozo/meusgastos/domain/repository/UsuarioRepository.java
+++ src/main/java/me/brunocardozo/meusgastos/domain/repository/UsuarioRepository.java
@@ -3,8 +3,8 @@ package me.brunocardozo.meusgastos.domain.repository;
 import me.brunocardozo.meusgastos.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    List<Usuario> findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
