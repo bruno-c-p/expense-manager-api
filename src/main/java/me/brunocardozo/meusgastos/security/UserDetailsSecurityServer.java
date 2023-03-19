87243995c87a55109b896e9ab7657030cd6b8405
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-03-19 14:35:18 -0300

    feat: UserDetailsSecurityServer

---
+++ src/main/java/me/brunocardozo/meusgastos/security/UserDetailsSecurityServer.java
@@ -0,0 +1,18 @@
+package me.brunocardozo.meusgastos.security;
+
+import me.brunocardozo.meusgastos.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsSecurityServer implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
