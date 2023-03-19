cbd824b26b24a7163415c780372e8cba574332eb
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-03-19 16:11:42 -0300

    feat: TituloRepository

---
+++ src/main/java/me/brunocardozo/meusgastos/domain/repository/TituloRepository.java
@@ -0,0 +1,6 @@
+package me.brunocardozo.meusgastos.domain.repository;
+
+import me.brunocardozo.meusgastos.domain.model.Titulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TituloRepository extends JpaRepository<Titulo, Long> {}
