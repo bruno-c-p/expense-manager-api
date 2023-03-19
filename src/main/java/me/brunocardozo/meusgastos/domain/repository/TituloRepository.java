ac0b0c2d688cfe0546e7c2668e389ea8becefb7e
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-03-19 17:30:59 -0300

    feat: Titulo findByUsuario and obterFluxoCaixaPorDataVencimento

--- src/main/java/me/brunocardozo/meusgastos/domain/repository/TituloRepository.java
+++ src/main/java/me/brunocardozo/meusgastos/domain/repository/TituloRepository.java
@@ -1,6 +1,15 @@
 package me.brunocardozo.meusgastos.domain.repository;
 
 import me.brunocardozo.meusgastos.domain.model.Titulo;
+import me.brunocardozo.meusgastos.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TituloRepository extends JpaRepository<Titulo, Long> {}
+import java.util.List;
+
+public interface TituloRepository extends JpaRepository<Titulo, Long> {
+    @Query(nativeQuery = true, value = "select * from titulo where data_vencimento between to_timestamp(:periodoInicial, 'DD/MM/YYYY') and to_timestamp(:periodoFinal, 'DD/MM/YYYY')")
+    List<Titulo> obterFluxoCaixaPorDataVencimento(@Param("periodoInicial") String periodoInicial, @Param("periodoFinal") String periodoFinal);
+    List<Titulo> findByUsuario(Usuario usuario);
+}
